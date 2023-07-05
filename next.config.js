1abc6d388ea3621cd94fdc16585a354f5c3aad5f
Pedp4 WPBX5105GRF052215120 <wpbx5105grf052215120@pedp4.onmicrosoft.com>
2023-07-06 01:15:39 +0600

    Update

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {}
 
 // module.exports = nextConfig
 module.exports = {
-    output: 'export',
+    // output: 'export',
     images: {
       unoptimized: true,
       remotePatterns: [
