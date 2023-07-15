b9fd962d0fc63978442d30ecee9bdc59a0a1489d
Pedp4 WPBX5105GRF052215120 <wpbx5105grf052215120@pedp4.onmicrosoft.com>
2023-07-15 19:57:34 +0600

    Update

--- app/feed/publication.js
+++ app/feed/publication.js
@@ -13,7 +13,7 @@ const Publications = () => {
                 <ul style={{listStyleType: "none"}}>
                     <li>
                     <p class="text-justify"><span class="text-sm font-semibold">📌 [September, 2023]</span> <span class="text-sky-700 font-bold">Rank Your Summaries: Enhancing Bengali Text Summarization via Ranking-based Approach</span></p>
-                    <p class="text-justify"><span class="font-semibold">Authors: </span> G. M. Shahariar*, <span class="font-semibold text-orange-500">Tonmoy Talukder</span>*, Rafin Alam Khan Sotez, Md. Tanvir Rouf Shawon</p> 
+                    <p class="text-justify"><span class="font-semibold">Authors: </span> G. M. Shahariar*, <span class="font-semibold text-orange-500">Tonmoy Talukder</span>*, Rafin Alam Khan Sotez, and Md. Tanvir Rouf Shawon</p> 
                     <span class="text-xs italic">(* denotes equal contribution, names are listed in alphabetical order).</span>
                     <p class="text-justify"><span class="text-sm font-semibold">Conference: </span> <span class="italic">International Conference on Big Data, IoT and Machine Learning (BIM 2023)</span></p>
                     <p class="text-justify">[<span class="text-sky-400 hover:text-orange-400">Abstract</span>] [<span class="text-sky-400 hover:text-orange-400">PDF</span>] [<span class="text-sky-400 hover:text-orange-400">Code & Dataset</span>] [<span class="text-sky-400 hover:text-orange-400">Presentation</span>] [<span class="text-sky-400 hover:text-orange-400">Citation bib</span>]</p>
