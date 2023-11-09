bfe3973240ca00dfb9aa14c461bef545be32445e
Pedp4 WPBX5105GRF052215120 <wpbx5105grf052215120@pedp4.onmicrosoft.com>
2023-11-09 22:50:43 +0600

    Update

--- app/feed/recent.js
+++ app/feed/recent.js
@@ -15,10 +15,11 @@ const Recent = () => {
         <hr/>
             <div>
             <ul style={{listStyleType: "none"}}>
-                {/* <li className='py-2'>
+                <li className='py-2'>
                 <p className="text-justify"><span className="text-sm font-semibold">[September, 2023]</span> One Paper <span style={{color: 'green'}}>published</span> at <span className="italic ">
-                <span className="text-sky-600 hover:text-orange-400 hover:underline"><a href="https://confbim.com/" target="_blank" rel="noreferrer">BIM 2023</a></span></span>, Springer Lecture Notes in Networks and Systems.</p>
-                </li> */}
+                <span className="text-sky-600 hover:text-orange-400 hover:underline"><a href="https://confbim.com/" target="_blank" rel="noreferrer">BIM 2023</a></span></span></p> 
+                {/* , Springer Lecture Notes in Networks and Systems. */}
+                </li>
                 <li className='py-2'>
                 <p className="text-justify"><span className="text-sm font-semibold">[July, 2023]</span> One Paper <span style={{color: 'blue'}}>accepted</span> at <span className="italic ">
                 <span className="text-sky-600 hover:text-orange-400 hover:underline"><a href="https://confbim.com/" target="_blank" rel="noreferrer">BIM 2023</a></span></span> conference.</p>
