da552209a8d12cf1b325b6cae54bdec5cedf65e6
Tonmoy Talukder <tonmoytalukder2000@gmail.com>
2024-02-12 21:01:56 +0600

    Update

--- app/feed/recent.js
+++ app/feed/recent.js
@@ -16,16 +16,19 @@ const Recent = () => {
             <div>
             <ul style={{listStyleType: "none"}}>
                 <li className='py-2'>
-                <p className="text-justify"><span className="text-sm font-semibold">[September, 2023]</span> One Paper <span style={{color: 'green'}}>published</span> at <span className="italic ">
-                <span className="text-sky-600 hover:text-orange-400 hover:underline"><a href="https://confbim.com/" target="_blank" rel="noreferrer">BIM 2023</a></span></span>.</p> 
-                {/* , Springer Lecture Notes in Networks and Systems. */}
+                  <p className="text-justify"><span className="text-sm font-semibold">[February, 2024]</span> I have <span style={{color: 'blue'}}>joined</span> <span className="text-sky-600 hover:text-orange-400 hover:underline"><a href="https://bd.linkedin.com/company/research-hub-21" target="_blank" rel="noreferrer">Reseach Hub.Inc</a></span> as a <bold class="font-medium">Research Assistant</bold> on a contractual basis.</p>
                 </li>
                 <li className='py-2'>
-                <p className="text-justify"><span className="text-sm font-semibold">[July, 2023]</span> One Paper <span style={{color: 'blue'}}>accepted</span> at <span className="italic ">
-                <span className="text-sky-600 hover:text-orange-400 hover:underline"><a href="https://confbim.com/" target="_blank" rel="noreferrer">BIM 2023</a></span></span> conference.</p>
+                  <p className="text-justify"><span className="text-sm font-semibold">[September, 2023]</span> One Paper <span style={{color: 'green'}}>published</span> at <span className="italic ">
+                  <span className="text-sky-600 hover:text-orange-400 hover:underline"><a href="https://confbim.com/" target="_blank" rel="noreferrer">BIM 2023</a></span></span>.</p> 
+                  {/* , Springer Lecture Notes in Networks and Systems. */}
                 </li>
                 <li className='py-2'>
-                <p className="text-justify"><span className="text-sm font-semibold">[August, 2022]</span> I have started <span style={{color: '#FF0000'}}>research</span> under the supervision of <span className="text-orange-700 hover:text-orange-400 hover:underline"><a href="https://shahariar-shibli.github.io/" target="_blank" rel="noreferrer">Mr. G. M. Shahariar Shibli</a></span>, Faculty, Department of Computer Science and Engineering, Ahsanullah University of Science and Technology.</p>
+                  <p className="text-justify"><span className="text-sm font-semibold">[July, 2023]</span> One Paper <span style={{color: 'blue'}}>accepted</span> at <span className="italic ">
+                  <span className="text-sky-600 hover:text-orange-400 hover:underline"><a href="https://confbim.com/" target="_blank" rel="noreferrer">BIM 2023</a></span></span> conference.</p>
+                </li>
+                <li className='py-2'>
+                  <p className="text-justify"><span className="text-sm font-semibold">[August, 2022]</span> I have started <span style={{color: '#FF0000'}}>research</span> under the supervision of <span className="text-orange-700 hover:text-orange-400 hover:underline"><a href="https://shahariar-shibli.github.io/" target="_blank" rel="noreferrer">Mr. G. M. Shahariar Shibli</a></span>, Faculty, Department of Computer Science and Engineering, Ahsanullah University of Science and Technology.</p>
                 </li>
             </ul>
             </div>
