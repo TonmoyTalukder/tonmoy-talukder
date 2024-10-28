e60cb32733e0b2db42b1dce5dab4aaf43f406118
Tonmoy Talukder <tonmoytalukder2000@gmail.com>
2024-10-28 20:03:22 +0600

    Dev page fix

--- app/components/dev-page/components/section-divider.tsx
+++ app/components/dev-page/components/section-divider.tsx
@@ -6,10 +6,14 @@ import { motion } from "framer-motion";
 export default function SectionDivider() {
   return (
     <motion.div
-      className="my-24 block"  //hidden sm:
+      className="block"  //hidden sm:
       initial={{ opacity: 0, y: 100 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ delay: 0.125 }}
+
+      style={{
+        marginBottom: '20vh'
+      }}
     >
       {/* Arrow Icon with Bouncing Animation */}
       <motion.svg
